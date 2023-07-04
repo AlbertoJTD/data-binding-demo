a8b91b440f284ef259a302e5780536181538b170
AlbertoJTD <albertojtd@outlook.com>
2023-07-04 10:32:01 -0600

    First steps with data binding

--- DataBindingDemo/DataBindingDemo/MainPage.xaml.cs
+++ DataBindingDemo/DataBindingDemo/MainPage.xaml.cs
@@ -13,6 +13,26 @@ namespace DataBindingDemo
 		public MainPage()
 		{
 			InitializeComponent();
+
+			//string message = "Hello";
+			//List<string> names = new List<string>
+			//{
+			//	"John",
+			//	"Jhonny",
+			//	"Sonya",
+			//	"Jason"
+			//};
+
+			var person = new Person()
+			{
+				Name = "John",
+				Address = "Great Av.",
+				Age = 67,
+				LastName = "Wick",
+				Phone = "5550010999"
+			};
+
+			this.BindingContext = person;
 		}
 	}
 }
